0b8c1cc6e99c3ef135802a63b5a88c64eeb92eb9
sultan232003 <121170561+sultan232003@users.noreply.github.com>
2024-08-02 22:52:18 +0530

    main.js

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -21,8 +21,16 @@
 
 const tilt = document.getElementsByClassName("tilt")
 
+function nest(parent, depth) {
+  if (depth === 0) return
+  const newdiv = document.createElement("div")
+  newdiv.classList.add("tilt_infinity_child")
+  parent.appendChild(newdiv)
+  nest(newdiv, depth - 1)
+}
+
 class Tilt {
-  constructor(tilt_card, tilt_strength, tilt_x, tilt_y, float, scale, startX, startY, reverse, page, reset) {
+  constructor(tilt_card, tilt_strength, tilt_x, tilt_y, float, scale, startX, startY, reverse, page, reset, infinity) {
     this.tilt_card = tilt_card
     this.tilt_strength = tilt_strength || 5
     this.tilt_x = tilt_x || 'true'
@@ -34,14 +42,28 @@ class Tilt {
     this.reverse = reverse || 'false'
     this.page = page || 'false'
     this.reset = reset || 'true'
+    this.infinity = infinity || 'false'
+    this.infinity_data = this.infinity.split(/\s+/).map(Number)
     this.card_data = tilt_card.getBoundingClientRect()
+    this.tilt_card_half_width = this.card_data.width / 2
+    this.tilt_card_half_height = this.card_data.height / 2
+    this.tilt_infinity_child
   }
-  
+
   update() {
-    document.addEventListener("scrollend",(e)=>{
+    document.addEventListener("scrollend", (e) => {
       this.card_data = this.tilt_card.getBoundingClientRect()
     })
+
     this.tilt_card.style.cssText = `transform-style: preserve-3d; transition: all 200ms ease ; transform: perspective(500px) rotateY(${this.startY}deg) rotateX(${this.startX}deg);`
+
+    if (this.infinity_data[0] > 0) {
+      nest(this.tilt_card, this.infinity_data[0])
+    }
+
+    this.tilt_infinity_child = document.getElementsByClassName("tilt_infinity_child")
+    this.tilt_infinity_child_size = Math.pow(this.infinity_data[2], 1 / this.infinity_data[0]) * 100;
+
     this.child = this.tilt_card.children
     Array.from(this.child).forEach(childs => {
       childs.style.cssText = `transform: translateZ(${this.float}px);`
@@ -57,6 +79,7 @@ class Tilt {
       } else {
         this.rotateX = 0
       }
+
       if (this.tilt_y === "true") {
         if (this.page === "true") {
           this.rotateY = ((e.clientX - (window.innerWidth / 2)) / this.tilt_strength)
@@ -73,9 +96,22 @@ class Tilt {
         this.final_rotateY = -this.rotateY
       }
 
-      this.tilt_card.style.cssText = `transform-style: preserve-3d; transform: scale(${this.scale})perspective(500px) rotateY(${this.final_rotateY}deg) rotateX(${this.final_rotateX}deg);`
-    }
+      if (this.infinity_data[0] > 0) {
+        this.overflow = "hidden"
+        this.infinity_moveY = ((e.clientY - this.card_data.y) - this.card_data.height / 2) / ((this.card_data.height / 2) / this.infinity_data[1])
+        this.infinity_moveX = ((e.clientX - this.card_data.x) - this.card_data.width / 2) / ((this.card_data.width / 2) / this.infinity_data[1])
+      } else {
+        this.overflow = "visible"
+      }
 
+      this.tilt_card.style.cssText = `overflow:${this.overflow}; transform-style: preserve-3d; transform: scale(${this.scale})perspective(500px) rotateY(${this.final_rotateY}deg) rotateX(${this.final_rotateX}deg);`
+
+      if (this.infinity_data[0] > 0) {
+        Array.from(this.tilt_infinity_child).forEach(tilt_infinity_childs => {
+          tilt_infinity_childs.style.cssText = `width:${this.tilt_infinity_child_size}% ; height:${this.tilt_infinity_child_size}% ; transform: perspective(500px) translateY(${this.infinity_moveY}%) translateX(${this.infinity_moveX}%);`
+        })
+      }
+    }
 
     if (this.page === "true") {
       document.addEventListener("mousemove", (e) => {
@@ -88,24 +124,20 @@ class Tilt {
     }
 
     this.tilt_card.addEventListener("mouseleave", () => {
+      Array.from(this.tilt_infinity_child).forEach(tilt_infinity_childs => {
+        tilt_infinity_childs.style.cssText = `width:${this.tilt_infinity_child_size}% ; height:${this.tilt_infinity_child_size}% ; transition: all 200ms ease ;transform: perspective(500px) translateY(0%) translateX(0%);`
+      })
+
       if (this.reset === "true") {
-        this.tilt_card.style.cssText = `transition: all 200ms ease ; transform: perspective(500px) rotateY(${this.startY}deg) rotateX(${this.startX}deg);`
+        this.tilt_card.style.cssText = `overflow:${this.overflow}; transition: all 200ms ease ; transform: perspective(500px) rotateY(${this.startY}deg) rotateX(${this.startX}deg);`
       }
     })
   }
-
-  // copy(){
-  //   this.tilt_card.addEventListener("click",(e)=>{
-  //     navigator.clipboard.writeText(this.tilt_card)
-  //   })
-  // }
 }
 
+let final_tilt
+
 Array.from(tilt).forEach(tilts => {
-  
-  tilts.addEventListener("click",(e)=>{
-     navigator.clipboard.writeText()
-  })
   cards_tilt_strength = tilts.getAttribute("tilt_strength")
   cards_tilt_x = tilts.getAttribute("tilt_x")
   cards_tilt_y = tilts.getAttribute("tilt_y")
@@ -116,17 +148,20 @@ Array.from(tilt).forEach(tilts => {
   cards_tilt_reverse = tilts.getAttribute("tilt_reverse")
   cards_tilt_page = tilts.getAttribute("tilt_page")
   cards_tilt_reset = tilts.getAttribute("tilt_reset")
-  tilts = new Tilt(tilts, cards_tilt_strength, cards_tilt_x, cards_tilt_y, cards_tilt_float, cards_tilt_scale, cards_tilt_StartX, cards_tilt_StartY, cards_tilt_reverse, cards_tilt_page, cards_tilt_reset)
-  tilts.update()
+  cards_tilt_infinity = tilts.getAttribute("tilt_infinity")
+  final_tilt = new Tilt(tilts, cards_tilt_strength, cards_tilt_x, cards_tilt_y, cards_tilt_float, cards_tilt_scale, cards_tilt_StartX, cards_tilt_StartY, cards_tilt_reverse, cards_tilt_page, cards_tilt_reset, cards_tilt_infinity)
+  final_tilt.update()
 });
 
-
+Array.from(final_tilt.tilt_infinity_child).forEach(tilt_infinity_childs => {
+  tilt_infinity_childs.style.cssText = `width:${final_tilt.tilt_infinity_child_size}% ; height:${final_tilt.tilt_infinity_child_size}% ;`
+})
 
 const Copy_btn = document.getElementsByClassName("copy_btn")
 Array.from(Copy_btn).forEach(Copy_btns => {
-  Copy_btns.addEventListener("click",(e)=>{
+  Copy_btns.addEventListener("click", (e) => {
     default_tilt_data = Copy_btns.getAttribute("default_tilt_data")
     second_tilt_data = Copy_btns.getAttribute("second_tilt_data")
-    navigator.clipboard.writeText(default_tilt_data +" "+ second_tilt_data)
+    navigator.clipboard.writeText(default_tilt_data + " " + second_tilt_data)
   })
 })
