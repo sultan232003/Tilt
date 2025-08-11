fe7eb8b0a5733a6239d96db7ce62cdf1ff2d8b49
_Sultan_Ali_ <121170561+sultan232003@users.noreply.github.com>
2025-08-12 00:43:08 +0530

    Utils.js

--- Assets/JS/Utils.js
+++ Assets/JS/Utils.js
@@ -863,9 +863,25 @@ function collectOnes(arr, compare, row) {
     return result;
 }
 
+function drawRoundedRect(x, y, width, height, radius) {
+  ctx.beginPath();
+  ctx.moveTo(x + radius, y);
+  ctx.lineTo(x + width - radius, y);
+  ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+  ctx.lineTo(x + width, y + height - radius);
+  ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+  ctx.lineTo(x + radius, y + height);
+  ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+  ctx.lineTo(x, y + radius);
+  ctx.quadraticCurveTo(x, y, x + radius, y);
+  ctx.closePath();
+  ctx.fill();
+}
+
 function drawPoint(p) {
     ctx.beginPath();
     ctx.arc(p.x, p.y, 10, 0, Math.PI * 2, false);
     ctx.fill();
     // ctx.closePath()
 }
+
